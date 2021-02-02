5b73b320b227dd1305bf092f1c46bde4941cfff2
Imnul <imnulhaqueruman18@gmail.com>
2021-02-02 15:14:58 +0600

    Truthy Falsy Values

---
+++ truthyFalse.js
@@ -0,0 +1,17 @@
+//const num = 2; condition is true for all positive and negative number without 0.
+//const name =" EMon "; condition is ture when name veriable assign any string or space but its false when it assign empty string
+//Falsy 
+// 0
+// ""
+// null
+//NaN
+// undefined
+//truthy
+// "0" " " [] {} 
+
+if(name1){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is False');;
+}
